b38a6b443b061a224feb94769e655d8e0cbf7c3b
katguerin <katguerin55@gmail.com>
2020-07-13 16:50:46 -0700

    up to 18.5.3

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,28 +6,28 @@ const pizzaController = {
   getAllPizza(req, res) {
     Pizza.find({})
       .populate({
-        path: 'comments',
-        select: '-__v'
+        path: "comments",
+        select: "-__v",
       })
-      .select('-__v')
+      .select("-__v")
       .sort({ _id: -1 })
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
         console.log(err);
         res.sendStatus(400);
       });
   },
 
-// get one pizza by id
-getPizzaById({ params }, res) {
+  // get one pizza by id
+  getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
       .populate({
-        path: 'comments',
-        select: '-__v'
+        path: "comments",
+        select: "-__v",
       })
-      .select('-__v')
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
+      .select("-__v")
+      .then((dbPizzaData) => res.json(dbPizzaData))
+      .catch((err) => {
         console.log(err);
         res.sendStatus(400);
       });
@@ -42,7 +42,10 @@ getPizzaById({ params }, res) {
 
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
@@ -50,7 +53,7 @@ getPizzaById({ params }, res) {
         }
         res.json(dbPizzaData);
       })
-      .catch((err) => res.status(400).json(err));
+      .catch((err) => res.json(err));
   },
 
   // delete pizza
