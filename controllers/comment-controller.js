3cee935c1a5113aff356e244fc86c71eb5ec8321
katguerin <katguerin55@gmail.com>
2020-07-16 19:24:26 -0700

    everything finally works!

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -73,21 +73,21 @@ const commentController = {
   },
 };
 
-const router = require("express").Router();
-const {
-  addComment,
-  removeComment,
-} = require("../../controllers/comment-controller");
-const commentRoutes = require("./comment-routes");
-const pizzaRoutes = require("./pizza-routes");
+// const router = require("express").Router();
+// const {
+//   addComment,
+//   removeComment,
+// } = require("../../controllers/comment-controller");
+// const commentRoutes = require("./comment-routes");
+// const pizzaRoutes = require("./pizza-routes");
 
-router.use("/comments", commentRoutes);
-router.use("/pizzas", pizzaRoutes);
+// router.use("/comments", commentRoutes);
+// router.use("/pizzas", pizzaRoutes);
 
-// /api/comments/<pizzaId>
-router.route("/:pizzaId").post(addComment);
+// // /api/comments/<pizzaId>
+// router.route("/:pizzaId").post(addComment);
 
-// /api/comments/<pizzaId>/<commentId>
-router.route("/:pizzaId/:commentId").delete(removeComment);
+// // /api/comments/<pizzaId>/<commentId>
+// router.route("/:pizzaId/:commentId").delete(removeComment);
 
-module.exports = router;
+module.exports = commentController;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -7,8 +7,11 @@ const {
   removeReply,
 } = require("../../controllers/comment-controller");
 
+router.route("/:pizzaId").post(addComment);
+
 router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
 
 router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
+
 
 module.exports = router;
